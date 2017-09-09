d0d2e2558403721fb4d368f47067424369b10d24
llcoolj84 <llcj84@gmail.com>
2017-09-09 22:39:01 +0000

    stretch

--- day1/password/password.js
+++ day1/password/password.js
@@ -13,8 +13,8 @@
 function obfuscate(password) {
 
   var password1 = password.toString();
-  var output = passwordInput.replace(/a/g, 4).replace(/e/g, 3).replace(/o/g, 0).replace(/l/g, 1);
+  var output = password1.replace(/a/g, 4).replace(/e/g, 3).replace(/o/g, 0).replace(/l/g, 1);
   return output;
 }
 
-console.log(obfuscate(process.argv.slice([2]));
+console.log(obfuscate(process.argv.slice([2])));
