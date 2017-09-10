4276757e871a23ac9792590ad4574599053fdb10
llcoolj84 <llcj84@gmail.com>
2017-09-10 05:26:01 +0000

    another commit for http exercise

--- day6/httpclientstream/http_example.js
+++ day6/httpclientstream/http_example.js
@@ -8,8 +8,10 @@ var https = require('https');
 console.log('I did it!');
 
 // called by https when the request is made.
-var callback = function() {
+// called by https when the request is made.
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
@@ -17,3 +19,4 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
